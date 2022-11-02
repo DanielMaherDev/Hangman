7dc661ee5c415ea08c160cc0525e3595e7090d35
DanielMaherDev <dmdanielmaher@gmail.com>
2022-11-02 16:25:26 +0000

    add text and input to start game function

--- run.py
+++ run.py
@@ -1,10 +1,38 @@
 import random
 from words import list_of_words
 
-print(list_of_words['easy'])
+def start_game():
+    print("""
+    
+ __ __   ____  ____    ____  ___ ___   ____  ____  
+|  |  | /    ||    \  /    ||   |   | /    ||    \ 
+|  |  ||  o  ||  _  ||   __|| _   _ ||  o  ||  _  |
+|  _  ||     ||  |  ||  |  ||  \_/  ||     ||  |  |
+|  |  ||  _  ||  |  ||  |_ ||   |   ||  _  ||  |  |
+|  |  ||  |  ||  |  ||     ||   |   ||  |  ||  |  |
+|__|__||__|__||__|__||___,_||___|___||__|__||__|__|
+                                                   
+
+    """)
+
+    print('Welcome to HangMan!\n')
+    print('What is your name?\n \n')
+    name = input('ENTER YOUR NAME:')
+    print(f"\n Welcome, {name}")
+    print("""
+    Would you like to:
+    1. Start Game
+    2. Read The Rules
+    3. View Leaderboard? \n \n""")
+    print('Please enter the number which corresponds to your selection! \n')
+    input('Number:')
 
 def main():
     """
     Run game functions
     """
+    start_game()
+
+
+main()
 
