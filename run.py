2e15882a8f6560297fae8c9a144412d4ab23fb0c
DanielMaherDev <dmdanielmaher@gmail.com>
2022-11-02 16:07:00 +0000

    add words file, and import to run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import random
+from words import list_of_words
+
+print(list_of_words['easy'])
 
 def main():
     """
     Run game functions
     """
+
---
+++ words.py
@@ -0,0 +1,3 @@
+list_of_words = {'easy': ['red', 'team', 'fly'], 
+                 'medium': ['carrot', 'domino'],
+                 'hard': ['dynasty', 'tempermental']}
