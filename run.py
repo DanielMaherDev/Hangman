c3f3724d2d1802b082d5e3811efa0d3eaac3f1a4
DanielMaherDev <dmdanielmaher@gmail.com>
2022-11-05 18:44:10 +0000

    wrap section in make guess function within a while statement

--- run.py
+++ run.py
@@ -156,54 +156,57 @@ def make_guess(guess_number, num_lives, word):
 
     print(word_blanks_as_list)
     game_over = False
-
-    print(HANGMAN[guess_number] + '\n')
-    print(f'Your word: {word_blank}')
-    print(f'You have {num_lives} lives! \n')
     while not game_over and num_lives > 0:
+        print(HANGMAN[guess_number] + '\n')
+        print(f'Your word: {word_blank}')
 
-        this_guess = input('Please choose a letter to guess:')
-        try:
-            if len(this_guess) > 1:
-                raise ValueError(
-                f'You can only guess 1 letter, but you guessed {len(this_guess)} characters! \n'
-                )
-            elif this_guess == "":
-                print('Please enter a guess')                    
-            elif (len(this_guess) == 1 and this_guess.isalpha() 
-                  and this_guess in guesses):
-                raise ValueError(
-                f'"{this_guess}" has already been guessed.'
-                )
-            elif not this_guess.isalpha():
-                raise ValueError(
-                f" Only letters are valid guesses, but you guessed {this_guess}"
-                )
-            elif this_guess not in word:
-                print(f"{this_guess} is not in the word. You lose a life!")
-                guesses.append(this_guess)
-                print(guess_number)
-                num_lives -= 1
-                guess_number += 1
-            else:
-                print(f'Great Guess! {this_guess} is in the word!')
-                guesses.append(this_guess)
-                x = 0
-                while x < len(word):
-                    if this_guess == word_as_list[x]:
-                        word_blanks_as_list[x] = this_guess
-                    x = x+1
-                word_blank = "".join(word_blanks_as_list)
-                if word_blank == word:
-                    game_over = True
-                        
-                        
-        except ValueError as e:
-            print(f"{e}.\n Please try again.\n")
-    if num_lives == 0:
-        print(f'Unlucky. You ran out of lives! The correct word was {word}')
-    else:
-        print('You win!')
+        print(f'You have {num_lives} lives! \n')
+        guess_made = False
+        while guess_made == False:
+            this_guess = input('Please choose a letter to guess:')
+            try:
+                if len(this_guess) > 1:
+                    raise ValueError(
+                    f'You can only guess 1 letter, but you guessed {len(this_guess)} characters! \n'
+                    )
+                elif this_guess == "":
+                    print('Please enter a guess')                    
+                elif (len(this_guess) == 1 and this_guess.isalpha() 
+                    and this_guess in guesses):
+                    raise ValueError(
+                    f'"{this_guess}" has already been guessed.'
+                    )
+                elif not this_guess.isalpha():
+                    raise ValueError(
+                    f" Only letters are valid guesses, but you guessed {this_guess}"
+                    )
+                elif this_guess not in word:
+                    print(f"{this_guess} is not in the word. You lose a life!")
+                    guesses.append(this_guess)
+                    print(guess_number)
+                    num_lives -= 1
+                    guess_number += 1
+                    guess_made = True
+                else:
+                    print(f'Great Guess! {this_guess} is in the word!')
+                    guesses.append(this_guess)
+                    guess_made = True
+                    x = 0
+                    while x < len(word):
+                        if this_guess == word_as_list[x]:
+                            word_blanks_as_list[x] = this_guess
+                        x = x+1
+                    word_blank = "".join(word_blanks_as_list)
+                    if word_blank == word:
+                        game_over = True
+                            
+                            
+            except ValueError as e:
+                print(f"{e}.\n Please try again.\n")
+        if num_lives == 0:
+            print(f'Unlucky. You ran out of lives! The correct word was {word}')
+        else:
+            print('You win!')
 
 def main():
     """
